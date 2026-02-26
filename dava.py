ba1740c8a3506c6b7a2775684e1954c8d180df85
T4M15 <temikotemi20@gmail.com>
2026-02-26 15:51:32 +0400

    Add files via upload

---
+++ dava.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name , surname , age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+
+object = Student("Temi" ,"Danelia" , 15)
+print(object.name , object.age)
+
+
